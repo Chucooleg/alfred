6caa137fd5115d16e71f4a91d54dac1e80df4c04
chucooleg <chucooleg@gmail.com>
2020-09-05 12:27:24 -0700

    split exp2 eval on 2 azvms

--- eval_by_agent_scripts/exp_2_eval_azvm_seen.sh
+++ eval_by_agent_scripts/exp_2_eval_azvm_seen.sh
@@ -7,11 +7,11 @@ export DOUT=$DATA_ROOT/exp/model:$MODEL,name:$MODEL_NAME
 echo $MODEL_NAME
 echo $DOUT
 
-# Training
+# # Training
 cd $ALFRED_ROOT
-echo Start training agent $MODEL_NAME
-echo Will Save to $DOUT...
-python -m memory_profiler models/train/train_seq2seq_agent.py --data $DATA --model $MODEL --dout $DOUT --splits $SPLITS --pp_folder $PP --gpu --batch 8 --pm_aux_loss_wt 0.1 --subgoal_aux_loss_wt 0.1 --save_every_epoch --epoch 27 --resume $DOUT/net_epoch_19.pth  --lr 1e-5
+# echo Start training agent $MODEL_NAME
+# echo Will Save to $DOUT...
+# python -m memory_profiler models/train/train_seq2seq_agent.py --data $DATA --model $MODEL --dout $DOUT --splits $SPLITS --pp_folder $PP --gpu --batch 8 --pm_aux_loss_wt 0.1 --subgoal_aux_loss_wt 0.1 --save_every_epoch --epoch 27 --resume $DOUT/net_epoch_19.pth  --lr 1e-5
 
 # Eval
 export EVAL_SPLITS=$DATA_ROOT/splits/oct21.json
@@ -27,15 +27,15 @@ echo 'Start Validation on Seen - Per Subgoal'
 python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3 --subgoals all
 
 
-# Eval on Validation Unseen
-echo 'Start Validation on Unseen'
-export AGENT_MODEL=$DOUT/net_epoch_19.pth
-export EVAL_SPLIT=valid_unseen
-python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3
+# # Eval on Validation Unseen
+# echo 'Start Validation on Unseen'
+# export AGENT_MODEL=$DOUT/net_epoch_19.pth
+# export EVAL_SPLIT=valid_unseen
+# python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3
 
-# Eval on Validation Unseen - Per Subgoal
-echo 'Start Validation on Unseen - Per Subgoal'
-python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3 --subgoals all
+# # Eval on Validation Unseen - Per Subgoal
+# echo 'Start Validation on Unseen - Per Subgoal'
+# python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3 --subgoals all
 
 
 # move agent model, tensorboard events and results to blob
