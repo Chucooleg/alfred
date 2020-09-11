acf6fe755802665ca4a2c6c3647dd64271f87586
chucooleg <chucooleg@gmail.com>
2020-09-10 19:37:45 -0700

    exp 3.1 eval script for azvm2

---
+++ eval_by_agent_scripts/exp_3.1_eval_azvm2.sh
@@ -0,0 +1,41 @@
+echo 'Within Exp 3 Sub Script'
+
+# Set output Directory
+export MODEL_NAME=experiment_3_original_train_set_halved_plus_other_half_annotated_by_$AUTOGENERATION_LANG_MODEL
+export DOUT=$DATA_ROOT/exp/model:$MODEL,name:$MODEL_NAME
+
+# Training
+cd $ALFRED_ROOT
+# echo Start training agent $MODEL_NAME
+# echo Will Save to $DOUT...
+# python models/train/train_seq2seq_agent.py --data $DATA --model $MODEL --dout $DOUT --splits $SPLITS --pp_folder $PP --gpu --batch 8 --pm_aux_loss_wt 0.1 --subgoal_aux_loss_wt 0.1 --use_autogeneration --autogeneration_data $DATA --autogeneration_lang_model $AUTOGENERATION_LANG_MODEL --save_every_epoch --epoch 27 --resume $DOUT/net_epoch_14.pth --lr 1e-4
+
+
+# Eval
+export EVAL_SPLITS=$DATA_ROOT/splits/oct21.json
+
+# Eval on Validation Seen
+echo 'Start Validation on Seen'
+export AGENT_MODEL=$DOUT/best_seen.pth
+export EVAL_SPLIT=valid_seen
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3
+
+# Eval on Validation Seen - Per Subgoal
+echo 'Start Validation on Seen - Per Subgoal'
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3 --subgoals all
+
+
+# Eval on Validation Unseen
+echo 'Start Validation on Unseen'
+export AGENT_MODEL=$DOUT/best_unseen.pth
+export EVAL_SPLIT=valid_unseen
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3
+
+# Eval on Validation Unseen - Per Subgoal
+echo 'Start Validation on Unseen - Per Subgoal'
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 3 --subgoals all
+
+
+# # move agent model, tensorboard events and results to blob
+# cp -r $DOUT $BLOB_EXP_DIR
+# echo Backed Up $DOUT to $BLOB_EXP_DIR
