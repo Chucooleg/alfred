0d56ec2c19b16301050fe7ecdfe04a5eb4d7eaf9
chucooleg <chucooleg@gmail.com>
2020-10-13 21:27:59 -0700

    adding test scripts

---
+++ eval_by_agent_scripts/agent_eval_exp1.1_azvm_test.sh
@@ -0,0 +1,24 @@
+# General Path Setup
+export ENV_HOME=/root/home/hoyeung
+export ALFRED_ROOT=$ENV_HOME/alfred
+export DATA_ROOT=/root/data_alfred
+export BLOB_ROOT=$ENV_HOME/blob_alfred_data/ # for backup to container
+export BLOB_EXP_DIR=$BLOB_ROOT/exp_all/
+export DATA=$DATA_ROOT/json_feat_2.1.0_backup_20200826_agent_training
+export PP=pp
+export MODEL=seq2seq_im_mask
+export AUGMENTATION_DATA=$DATA_ROOT/json_data_augmentation_20200820
+
+# where this script is
+export SCRIPT_HOME=$ALFRED_ROOT/eval_by_agent_scripts/
+cd $SCRIPT_HOME
+
+
+echo '-------------------------------'
+echo 'EVAL Exp 1.1 Seen Main Script'
+# 1.1 With failure annotation by the explainer
+export AUGMENTATION_LANG_MODEL=explainer
+# eval_script
+bash $SCRIPT_HOME/exp_1.1_eval_azvm_test.sh 
+
+
---
+++ eval_by_agent_scripts/exp_1.1_eval_azvm_test.sh
@@ -0,0 +1,23 @@
+echo 'WIthin Exp 1 Sub Script'
+
+# Set output Directory
+export MODEL_NAME=experiment_1_original_train_set_plus_all_failures_annotated_by_$AUGMENTATION_LANG_MODEL
+export DOUT=$DATA_ROOT/exp/model:$MODEL,name:$MODEL_NAME
+
+echo $MODEL_NAME
+echo $DOUT
+
+# # Training
+cd $ALFRED_ROOT
+# echo Start training agent $MODEL_NAME
+# echo Will Save to $DOUT...
+# python -m memory_profiler models/train/train_seq2seq_agent.py --data $DATA --model $MODEL --dout $DOUT --splits $SPLITS --pp_folder $PP --gpu --batch 8 --pm_aux_loss_wt 0.1 --subgoal_aux_loss_wt 0.1 --save_every_epoch --epoch 27 --resume $DOUT/net_epoch_19.pth  --lr 1e-5
+
+# Eval
+export EVAL_SPLITS=$DATA_ROOT/splits/oct21.json
+
+# Eval on Test
+echo 'Start Validation on Test'
+export AGENT_MODEL=$DOUT/net_epoch_23.pth
+python models/eval/leaderboard.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --model models.model.seq2seq_im_mask --gpu --num_threads 5
+
