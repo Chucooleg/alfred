3e61e820ceb5e3ab33083748e7c4f72c1a706c0b
chucooleg <chucooleg@gmail.com>
2020-12-10 13:49:54 -0800

    Debug collect obj states

--- gen/scripts/collect_demo_object_states.py
+++ gen/scripts/collect_demo_object_states.py
@@ -331,7 +331,7 @@ class CollectStates(EvalTask):
         return states, outpath
 
 def main(args, splits_to_thread_dict, thread_i=0):
-    print('hit main.')
+    import pdb; pdb.set_trace()
 
     raw_splits = splits_to_thread_dict[thread_i]
 
