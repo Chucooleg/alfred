19e952fceafd364a973977599c681aa6b47d630a
Ubuntu <hoyeung@legg-gpuvm.tnvq1pxttozevgzozjpi0lzr5g.xx.internal.cloudapp.net>
2021-01-11 04:13:59 +0000

    updating agent eval log file name

--- eval_by_agent_scripts/exp_1_eval_azvm.sh
+++ eval_by_agent_scripts/exp_1_eval_azvm.sh
@@ -21,19 +21,19 @@ export EVAL_SPLITS=$DATA_ROOT/splits/oct21.json
 echo 'Start Validation on Seen model epoch' $SEEN_EPOCH
 export AGENT_MODEL=$DOUT/net_epoch_$SEEN_EPOCH.pth
 export EVAL_SPLIT=valid_seen
-python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 | tee $DOUT/$AUGMENTATION_LANG_MODEL:seen_epoch$SEEN_EPOCH
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 | tee $DOUT/EvalLog_$AUGMENTATION_LANG_MODEL:seen_epoch$SEEN_EPOCH.txt
 
 # Eval on Validation Seen - Per Subgoal
 echo 'Start Validation on Seen - Per Subgoal - model epoch' $SEEN_EPOCH
-python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 --subgoals all | tee $DOUT/$AUGMENTATION_LANG_MODEL:seen_persubgoal_epoch$SEEN_EPOCH
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 --subgoals all | tee $DOUT/EvalLog_$AUGMENTATION_LANG_MODEL:seen_persubgoal_epoch$SEEN_EPOCH.txt
 
 
 # Eval on Validation Unseen
 echo 'Start Validation on Unseen model epoch' $UNSEEN_EPOCH
 export AGENT_MODEL=$DOUT/net_epoch_$UNSEEN_EPOCH.pth
 export EVAL_SPLIT=valid_unseen
-python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 | tee $DOUT/$AUGMENTATION_LANG_MODEL:unseen_epoch$UNSEEN_EPOCH
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 | tee $DOUT/EvalLog_$AUGMENTATION_LANG_MODEL:unseen_epoch$UNSEEN_EPOCH.txt
 
 # Eval on Validation Unseen - Per Subgoal
 echo 'Start Validation on Unseen - Per Subgoal - model epoch' $UNSEEN_EPOCH
-python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 --subgoals all | tee $DOUT/$AUGMENTATION_LANG_MODEL:unseen_persubgoal_epoch$UNSEEN_EPOCH
+python models/eval/eval_seq2seq_agent.py --model_path $AGENT_MODEL --data $DATA --splits $EVAL_SPLITS --eval_split $EVAL_SPLIT --model models.model.seq2seq_im_mask --gpu --num_threads 8 --subgoals all | tee $DOUT/EvalLog_$AUGMENTATION_LANG_MODEL:unseen_persubgoal_epoch$UNSEEN_EPOCH.txt
